f17fee3821cfa421171e2ecb8096db1ab44f688b
TemiBlake <temiloluwasomefun@gmail.com>
2023-10-16 13:30:35 -0700

    variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Smile entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
